9132898a7d210180dadeed5e2068dbc9e8439054
nhudo267 <nhudo164@gmail.com>
2020-04-19 20:51:27 +0700

    creating userSchema

--- server/app.js
+++ server/app.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI}= require('./keys')
 
+
+require('./models/user')
+
+
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 })
 mongoose.connection.on('connected',()=>{
-    console.log("connected to mongo")
+    console.log("connected to mongo yeahhh")
 })
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
