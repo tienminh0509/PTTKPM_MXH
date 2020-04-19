528c13a002eb0d4bed5986bd3d4d55c7ba82e3e1
nhudo267 <nhudo164@gmail.com>
2020-04-19 22:15:23 +0700

    postting data to database

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 router.get('/',(req,res)=>{
     res.send("Test")
@@ -9,10 +10,30 @@ router.get('/',(req,res)=>{
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
     if(!email || !password || name ){
-     return  res.status(422).json({error:"Please add all the fields"
-        })
+     return  res.status(422).json({error:"Please add all the fields"})
     }
-    res.json({message:"successfuly posted"})
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"user already exists with that email"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfuly"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+
+    })
 })
 
 module.exports = router
