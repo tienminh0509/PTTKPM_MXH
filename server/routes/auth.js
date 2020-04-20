3ab6cce0e096ecde6f4c30a50b02d543189b3b41
nhudo267 <nhudo164@gmail.com>
2020-04-20 23:19:11 +0700

    Creating signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -40,6 +40,33 @@ router.post('/signup',(req,res)=>{
       console.log(err)
     })
   })
+
+
+  router.post('/signin',(req,res)=>{
+    const {email,password}= req.body
+    if(!email || !password ){
+      res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(saveUser=>{
+      if(!saveUser){
+        res.status(422).json({error:"Invalid Email or password"})
+
+      }
+      bcrypt.compare(password,saveUser.password)
+      .then(doMatch=>{
+        if(doMatch){
+          res.json({message:"successful signed in"})
+        }
+        else{
+          return res.status(422).json({error:"Invalid password"})
+        }
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+    })
+  })
   
 
 module.exports = router
