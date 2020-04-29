9f9668e78fd4352f9ff3752dab7b840b65de5e51
nhudo267 <nhudo164@gmail.com>
2020-04-29 09:55:59 +0700

    view all posts route

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,11 +5,21 @@ const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model('Post')
 
 
+router.get('/allpost',(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts =>{
+        res.json({posts})
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+})
 
 router.post('/createpost',requireLogin,(req,res)=>{
     const {title,body,pic} = req.body 
     if(!title || !body ){
-      return  res.status(422).json({error:"Plase add all the fields"})
+      return  res.status(422).json({error:"Please add all the fields"})
     }
     req.user.password = undefined
     const post = new Post({
