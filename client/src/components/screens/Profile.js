a28c5c114596da07d97021d7e2cdfdca4e4816b7
nhudo267 <nhudo164@gmail.com>
2020-05-10 18:15:37 +0700

    Profile page UI

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -4,7 +4,38 @@ import React from 'react'
 
 const Profile = ()=>{
     return (
-        <h1>profile</h1>
+        <div style={{maxWidth:"550px",margin:"0px auto"}}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey",
+            }}>
+                <div>
+                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                    src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    />
+                </div>
+                <div>
+                    <h4>Baby Cute</h4>
+                    <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+                        <h6>1 posts</h6>
+                        <h6>1 follower</h6>
+                        <h6>1 following</h6>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="gallery">
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+                <img className="item"   src="https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
+            </div>
+        </div>
+        
     )
 }
 
