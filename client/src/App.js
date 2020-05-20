fa6867c527c9feeeb1f17ade998e8b5d4ebdb734
nhudo267 <nhudo164@gmail.com>
2020-05-20 11:03:29 +0700

    adding context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React,{useEffect,createContext,useReducer,useContext} from 'react';
 import NavBar from './components/Navbar'
 import "./App.css"
-import {BrowserRouter,Route} from 'react-router-dom'
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
 import Home from './components/screens/Home'
 import Signin from './components/screens/SignIn'
 import Profile from './components/screens/Profile'
 import Signup from './components/screens/Signup'
 import CreatePost from './components/screens/CreatePost'
+import {reducer,initialState} from './reducers/userReducer'
 
-function App() {
-  return (
-    <BrowserRouter>
+export  const   UserContext = createContext()
 
-      <NavBar />
+const Routing = ()=>{
+  const history = useHistory()
+  const {state,dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user =JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch({type:"USER",payload:user})
+      history.push('/')
+    }else{
+      history.push('/signin')
+    }
+  },[])
+  return(
+    <Switch>
       <Route exact  path="/" >
         <Home />
       </Route>
@@ -28,8 +40,22 @@ function App() {
       <Route path="/create">
         <CreatePost />
       </Route>
+    </Switch>
+  )
+}
+
+function App() {
+  const  [state,dispatch] = useReducer(reducer,initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+
+      <NavBar />
+
+       <Routing/>
+
     </BrowserRouter>
-      
+    </UserContext.Provider>
       
   );
 }
