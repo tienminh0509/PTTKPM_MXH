fb4de28fee1a19fc76e77c73d9eb2eddeeb08381
nhudo267 <nhudo164@gmail.com>
2020-05-20 22:14:43 +0700

    Fetching posts and showing that on Home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,34 +1,40 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 
 
 const Home = ()=>{
+    const [data,setData] = useState([])
+    useEffect(() => {
+        fetch('/allpost',{
+            headers: {
+                "Authorization" : "Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res =>res.json())
+        .then(result=>{   
+            setData(result.posts)
+        })
+    },[])
     return (
         <div className="home">
-            <div className="card home-card">
-                <h5>nhudo</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
+            {
+                data.map(item =>{
+                    return (
+                        <div className="card home-card" key={item._id}>
+                        <h5>{item.postedBy.name}</h5>
+                        <div className="card-image">
+                            <img src={item.photo}/> 
+                        </div>
+                        <div className="card-content">
+                        <i className="material-icons" style={{color:"red"}}>favorite</i>
+                        <h6>{item.title}</h6>
+                        <p>{item.body}</p>
+                        <input type="text" placeholder="add a comment"/>
                 </div>
-                <div className="card-content">
-                <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-                <div className="card home-card">
-                <h5>nhudo</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/> 
-                </div>
-                <div className="card-content">
-                <i className="material-icons" style={{color:"red"}}>favorite</i>
-                    <h6>title</h6>
-                    <p>this is amazing post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
-            </div>
+            </div> 
+                    )
+                })
+            }
+           
         </div>
         
     )
